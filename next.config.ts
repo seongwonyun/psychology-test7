1e35cda91d32149a364a0ca2ff336c800397f961
seongwonyun <tjddnjsekd@gmail.com>
2025-10-31 18:13:25 +0900

    최종

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  typescript: {
+    // !! 경고 !!
+    // 프로덕션 빌드 중 타입 오류가 발생하더라도 빌드가 성공적으로 완료되도록 허용합니다.
+    // !! 경고 !!
+    ignoreBuildErrors: true,
+  },
+};
+
 export default nextConfig;
